a60829ab6f02e6abaee5f9004d85f4eb3ddaa802
Sergey Filatyev <filatevsergey2407@gmail.com>
2022-12-15 16:28:59 +0200

    app-corrected

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import SearchBar from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
@@ -9,6 +8,7 @@ import s from './App.module.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { animateScroll as scroll } from 'react-scroll';
+import { useState, useEffect } from 'react';
 
 const Status = {
   IDLE: 'idle',
@@ -16,82 +16,147 @@ const Status = {
   RESOLVED: 'resolved',
   REJECTED: 'rejected',
 };
-class App extends Component {
-  state = {
-    error: null,
-    images: [],
-    page: 1,
-    imageName: '',
-    totalPages: null,
-    status: Status.IDLE,
-  };
-  handleFormSubmit = imageName => {
-    if (imageName === this.state.imageName) {
-      toast.info(`Image ${imageName} already uploaded`);
+export default function App() {
+  const [error, setError] = useState(null);
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [imageName, setImageName] = useState('');
+  const [totalPages, setTotalPages] = useState(null);
+  const [status, setStatus] = useState(Status.IDLE);
+  const handleFormSubmit = newImageName => {
+    if (newImageName === imageName) {
+      toast.info(`Image ${newImageName} already uploaded`);
       return;
     }
-    this.setState({
-      imageName,
-      page: 1,
-      images: [],
-    });
+    setImageName(newImageName);
+    setPage(1);
+    setImages([]);
   };
-
-  componentDidUpdate(_, prevState) {
-    if (
-      prevState.imageName !== this.state.imageName ||
-      this.state.page !== prevState.page
-    ) {
-      this.setState({ status: Status.PENDING });
-      this.renderImages();
+  useEffect(() => {
+    if (!imageName) {
+      return;
     }
-  }
-  renderImages() {
-    const { page, imageName } = this.state;
+    setStatus(Status.PENDING);
+    renderImages();
+  }, [page, imageName]);
 
+  const renderImages = () => {
     fetchImage(imageName, page)
       .then(response => {
         if (response.hits.length === 0) {
           toast.warning(
             `No images ${imageName} your search query. Please try again.`
           );
-          this.setState({ status: Status.IDLE });
+          setStatus(Status.IDLE);
           return;
         }
-        this.setState(prevState => ({
-          images: [...prevState.images, ...response.hits],
-          totalPages: Math.ceil(response.totalHits / 12),
-          status: Status.RESOLVED,
-        }));
+        setImages(prevImages => [...prevImages, ...response.hits]);
+        setTotalPages(Math.ceil(response.totalHits / 12));
+        setStatus(Status.RESOLVED);
       })
-      .catch(error => this.setState({ error, status: Status.REJECTED }));
-  }
-  nextPages = () => {
-    this.setState(({ page }) => ({
-      page: page + 1,
-    }));
+      .catch(error => {
+        setError(error);
+        setStatus(Status.REJECTED);
+      });
+  };
+  const nextPages = () => {
+    setPage(prevPage => prevPage + 1);
     scroll.scrollToBottom();
   };
-  render() {
-    const { status, error, images, page, totalPages } = this.state;
-
-    return (
-      <div className={s.App}>
-        <SearchBar onSubmit={this.handleFormSubmit} />
-        {status === Status.IDLE && (
-          <p className={s.text}>Please enter your search</p>
-        )}
-        {status === Status.PENDING && <Loader />}
-        {status === Status.RESOLVED && <ImageGallery images={images} />}
-        {status === Status.RESOLVED && page < totalPages && (
-          <Button onClick={this.nextPages} />
-        )}
-        {status === Status.REJECTED && (
-          <ImagesErrorView message={error.message} />
-        )}
-        <ToastContainer position="top-center" autoClose={3000} />
-      </div>
-    );
-  }
+  return (
+    <div className={s.App}>
+      <SearchBar onSubmit={handleFormSubmit} />
+      {status === Status.IDLE && (
+        <p className={s.text}>Please enter your search</p>
+      )}
+      {status === Status.PENDING && <Loader />}
+      {status === Status.RESOLVED && <ImageGallery images={images} />}
+      {status === Status.RESOLVED && page < totalPages && (
+        <Button onClick={nextPages} />
+      )}
+      {status === Status.REJECTED && (
+        <ImagesErrorView message={error.message} />
+      )}
+      <ToastContainer position="top-center" autoClose={3000} />
+    </div>
+  );
 }
-export default App;
+// class App extends Component {
+//   state = {
+//     error: null,
+//     images: [],
+//     page: 1,
+//     imageName: '',
+//     totalPages: null,
+//     status: Status.IDLE,
+//   };
+//   handleFormSubmit = imageName => {
+//     if (imageName === this.state.imageName) {
+//       toast.info(`Image ${imageName} already uploaded`);
+//       return;
+//     }
+//     this.setState({
+//       imageName,
+//       page: 1,
+//       images: [],
+//     });
+//   };
+
+//   componentDidUpdate(_, prevState) {
+//     if (
+//       prevState.imageName !== this.state.imageName ||
+//       this.state.page !== prevState.page
+//     ) {
+//       this.setState({ status: Status.PENDING });
+//       this.renderImages();
+//     }
+//   }
+//   renderImages() {
+//     const { page, imageName } = this.state;
+
+//     fetchImage(imageName, page)
+//       .then(response => {
+//         if (response.hits.length === 0) {
+//           toast.warning(
+//             `No images ${imageName} your search query. Please try again.`
+//           );
+//           this.setState({ status: Status.IDLE });
+//           return;
+//         }
+//         this.setState(prevState => ({
+//           images: [...prevState.images, ...response.hits],
+//           totalPages: Math.ceil(response.totalHits / 12),
+//           status: Status.RESOLVED,
+//         }));
+//       })
+//       .catch(error => this.setState({ error, status: Status.REJECTED }));
+//   }
+//   nextPages = () => {
+//     this.setState(({ page }) => ({
+//       page: page + 1,
+//     }));
+//     scroll.scrollToBottom();
+//   };
+//   render() {
+//     const { status, error, images, page, totalPages } = this.state;
+
+//     return (
+//       <div className={s.App}>
+//         <SearchBar onSubmit={this.handleFormSubmit} />
+//         {status === Status.IDLE && (
+//           <p className={s.text}>Please enter your search</p>
+//         )}
+//         {status === Status.PENDING && <Loader />}
+//         {status === Status.RESOLVED && <ImageGallery images={images} />}
+//         {status === Status.RESOLVED && page < totalPages && (
+//           <Button onClick={this.nextPages} />
+//         )}
+//         {status === Status.REJECTED && (
+//           <ImagesErrorView message={error.message} />
+//         )}
+//         <ToastContainer position="top-center" autoClose={3000} />
+//       </div>
+//     );
+//   }
+// }
+// export default App;
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -44,6 +44,7 @@ export default function SearchBar({ onSubmit }) {
 SearchBar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
+
 // class SearchBar extends Component {
 //   state = {
 //     imageName: '',
