fa7941389a7dfc10cf4a6efadd2278692435c431
Sergey Filatyev <filatevsergey2407@gmail.com>
2022-12-15 15:25:39 +0200

    progressing

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,37 +1,63 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'components/Modal/Modal';
 import s from './ImageGalleryItem.module.css';
-class ImageGalleryItem extends Component {
-  state = {
-    showModal: false,
+
+export default function ImageGalleryItem({ src, alt, largeImageUrl }) {
+  const [showModal, setShowModal] = useState(false);
+  const toggleModal = () => {
+    setShowModal(showModal => !showModal);
   };
-  static propTypes = {
-    src: PropTypes.string.isRequired,
-    alt: PropTypes.string.isRequired,
-    largeImageUrl: PropTypes.string.isRequired,
-  };
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
-  };
-  render() {
-    const { src, alt, largeImageUrl } = this.props;
-    const { showModal } = this.state;
-    return (
-      <li className={s.imageGalleryItem}>
-        <img
-          onClick={this.toggleModal}
-          className={s.imageGalleryItemImage}
-          src={src}
-          alt={alt}
-        />{' '}
-        {showModal && (
-          <Modal onClose={this.toggleModal} src={largeImageUrl} alt={alt} />
-        )}
-      </li>
-    );
-  }
+  return (
+    <li className={s.imageGalleryItem}>
+      <img
+        onClick={toggleModal}
+        className={s.imageGalleryItemImage}
+        src={src}
+        alt={alt}
+      />{' '}
+      {showModal && (
+        <Modal onClose={toggleModal} src={largeImageUrl} alt={alt} />
+      )}
+    </li>
+  );
 }
-export default ImageGalleryItem;
+ImageGalleryItem.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+  largeImageUrl: PropTypes.string.isRequired,
+};
+
+// class ImageGalleryItem extends Component {
+//   state = {
+//     showModal: false,
+//   };
+//   static propTypes = {
+//     src: PropTypes.string.isRequired,
+//     alt: PropTypes.string.isRequired,
+//     largeImageUrl: PropTypes.string.isRequired,
+//   };
+//   toggleModal = () => {
+//     this.setState(({ showModal }) => ({
+//       showModal: !showModal,
+//     }));
+//   };
+//   render() {
+//     const { src, alt, largeImageUrl } = this.props;
+//     const { showModal } = this.state;
+//     return (
+//       <li className={s.imageGalleryItem}>
+//         <img
+//           onClick={this.toggleModal}
+//           className={s.imageGalleryItemImage}
+//           src={src}
+//           alt={alt}
+//         />{' '}
+//         {showModal && (
+//           <Modal onClose={this.toggleModal} src={largeImageUrl} alt={alt} />
+//         )}
+//       </li>
+//     );
+//   }
+// }
+// export default ImageGalleryItem;
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,43 +1,75 @@
-import { Component } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import s from './Modal.module.css';
+import { useEffect } from 'react';
+
 const modalRoot = document.querySelector('#modal-root');
-class Modal extends Component {
-  static propTypes = {
-    onClose: PropTypes.func.isRequired,
-    src: PropTypes.string.isRequired,
-    alt: PropTypes.string.isRequired,
-  };
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
+export default function Modal({ alt, src, onClose }) {
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
+  const handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      onClose();
     }
   };
-
-  handleBackdropClick = event => {
-    if (event.currentTarget === event.target) {
-      this.props.onClose();
-    }
-  };
-  render() {
-    const { alt, src } = this.props;
-    return createPortal(
-      <div className={s.overlay} onClick={this.handleBackdropClick}>
-        <div className={s.modal}>
-          <img src={src} alt={alt} />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
+  return createPortal(
+    <div className={s.overlay} onClick={handleBackdropClick}>
+      <div className={s.modal}>
+        <img src={src} alt={alt} />
+      </div>
+    </div>,
+    modalRoot
+  );
 }
-export default Modal;
+Modal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+};
+// class Modal extends Component {
+//   static propTypes = {
+//     onClose: PropTypes.func.isRequired,
+//     src: PropTypes.string.isRequired,
+//     alt: PropTypes.string.isRequired,
+//   };
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   handleBackdropClick = event => {
+//     if (event.currentTarget === event.target) {
+//       this.props.onClose();
+//     }
+//   };
+//   render() {
+//     const { alt, src } = this.props;
+//     return createPortal(
+//       <div className={s.overlay} onClick={this.handleBackdropClick}>
+//         <div className={s.modal}>
+//           <img src={src} alt={alt} />
+//         </div>
+//       </div>,
+//       modalRoot
+//     );
+//   }
+// }
+// export default Modal;
